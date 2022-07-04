f5ecc164bff907a640aef6233b82ee10590925d6
Shahnawaz Alam <89523564+shahnawaz-alam37@users.noreply.github.com>
2022-07-04 13:03:25 +0530

    add /(slash) to routers

--- router/router.go
+++ router/router.go
@@ -8,10 +8,10 @@ import (
 func Router() *mux.Router{
 	router := mux.NewRouter()
 
-	router.HandleFunc("api/movies",controller.Getmyallmovies).Methods("GET")
-	router.HandleFunc("api/movie",controller.CreateMovie).Methods("POST")
-	router.HandleFunc("api/movie/{id}",controller.Markaswatched).Methods("PUT")
-	router.HandleFunc("api/movie/{id}",controller.Deletemymovie).Methods("DELETE")
-	router.HandleFunc("api/movie-delete-all",controller.Deletemyallmovie).Methods("DELETE")
+	router.HandleFunc("/api/movies",controller.Getmyallmovies).Methods("GET")
+	router.HandleFunc("/api/movie",controller.CreateMovie).Methods("POST")
+	router.HandleFunc("/api/movie/{id}",controller.Markaswatched).Methods("PUT")
+	router.HandleFunc("/api/movie/{id}",controller.Deletemymovie).Methods("DELETE")
+	router.HandleFunc("/api/movie-delete-all",controller.Deletemyallmovie).Methods("DELETE")
 	return router
 }
